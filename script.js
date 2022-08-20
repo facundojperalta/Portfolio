eca2ab5a72e4ff4b5b71f5d037f7a2ad1d736a8c
Facundo Javier Peralta <76714825+facundojperalta@users.noreply.github.com>
2022-08-20 14:25:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+gsap.to(
+  ".box", {
+    backgroundImage:"radial-gradient(at 82% 69%, hsla(36,86%,72%,1) 0, transparent 53%), radial-gradient(at 50% 76%, hsla(219,66%,76%,1) 0, transparent 58%), radial-gradient(at 26% 53%, hsla(267,77%,63%,1) 0, transparent 49%), radial-gradient(at 30% 46%, hsla(126,91%,78%,1) 0, transparent 58%), radial-gradient(at 91% 83%, hsla(310,69%,61%,1) 0, transparent 58%), radial-gradient(at 57% 6%, hsla(185,68%,61%,1) 0, transparent 44%), radial-gradient(at 29% 36%, hsl(357, 95%, 64%, 1) 0, transparent 47%)", duration:200, repeat:1
+  }
+);
